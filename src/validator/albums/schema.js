8bfafaa8a3f851a3b1e82d89519429767d109875
RazinSyakib43 <muhammadrazin14@gmail.com>
2024-03-14 17:18:13 +0700

    delete sub project to init consumer project

--- src/validator/albums/schema.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const AlbumPayloadSchema = Joi.object({
-  name: Joi.string().required(),
-  year: Joi.number().integer().required(),
-  coverUrl: Joi.string(),
-});
-
-module.exports = { AlbumPayloadSchema };
